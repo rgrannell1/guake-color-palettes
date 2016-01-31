a8531b55f8d162a6f7b7762ae31f301943076188
rgrannell1 <r.grannell2@gmail.com>
2016-01-31 19:20:19 +0000

    readded names to images

--- scripts/capture-screenshots.sh
+++ scripts/capture-screenshots.sh
@@ -1,7 +1,7 @@
 
 function example {
 
-	echo '{"name":"guake-color-palettes","version":"0.1.0","description":"command-line wrapper for varamenous guake colour pallete generator.","preferGlobal":true,"engines":{"node":">=4.0.0"},"contributors":[{"name":"Ryan Grannell","email":"r.grannell@gmail.com"}],"main":"cli.js","scripts":{"postinstall":""},"repository":{"type":"git","url":"git+https://github.com/rgrannell1/guake-color-palettes.git"},"bin":{"guake-cl":"src/cli/cli.js"},"author":"Ryan Grannell","license":"MIT","bugs":{"url":"https://github.com/rgrannell1/guake-color-palettes/issues"},"homepage":"https://github.com/rgrannell1/guake-color-palettes#readme","dependencies":{"babel-eslint":"^5.0.0-beta8","babel-polyfill":"^6.3.14","docopt":"^0.6.2","eslint":"^1.10.3"}}' | jq .
+	echo '{"name":"guake-color-palettes","description":"command-line wrapper for varamenous guake colour palette generator.","bin":{"guake-cl":"src/cli/cli.js"},"homepage":"https://github.com/rgrannell1/guake-color-palettes#readme"}' | jq .
 
 }
 
